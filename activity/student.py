44292b9aad6e97a6dba721c05481f77ced4a1b6c
Renata <murzinarenata@gmail.com>
2025-03-27 11:23:46 -0700

    implemented

--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,26 @@
-# add your Student class here!
+class Student:
+    def __init__(self, name, level, classes):
+        self.name = name
+        self.level = level
+        self.classes = classes
+
+    def add_class(self, new_class):
+        self.classes.append(new_class)
+
+        return self.classes
+
+    def get_num_classes(self):
+
+        num_classes = len(self.classes)
+        self.length = num_classes
+        return num_classes
+    
+    def summary(self):
+
+        print(f'{self.name} is a {self.level} enrolled in {self.length} classes')
+
+
+
+
+
+
